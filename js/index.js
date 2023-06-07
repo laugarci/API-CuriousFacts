9494e6e7151548f1cd55cc2d9edd723e1cdace92
Laura <garch.laura@gmail.com>
2023-06-07 09:26:41 +0200

    circulo de fondo y efecto de maquina de escribir

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
 const funFact = document.getElementById("fun-fact");
 const btnfunFact = document.getElementById("fun-fact-btn");
 
-function getfunFact () {
-    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
-    .then(response => {
-   if (!response.ok) {
-        throw new Error ("Error HTTP: " + response.status);
+function typeWriterEffect(text, speed) {
+  let i = 0;
+  const interval = setInterval(() => {
+    funFact.textContent += text.charAt(i);
+    i++;
+    if (i > text.length) {
+      clearInterval(interval);
     }
-    return response.json();
+  }, speed);
+}
+
+function getfunFact() {
+  fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Error HTTP: " + response.status);
+      }
+      return response.json();
     })
-    .then (data => {
-        funFact.textContent = data.text;
+    .then(data => {
+      funFact.textContent = "";
+      typeWriterEffect(data.text, 50);
     })
     .catch(error => {
-        console.log("error:", error);
-    })
+      console.log("error:", error);
+    });
 }
 
 btnfunFact.addEventListener("click", getfunFact);
