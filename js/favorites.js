98102b83798ee7fd1bdb40bde176fe74aaadea8d
Laura <garch.laura@gmail.com>
2023-06-08 10:14:03 +0200

    favs y scroll

--- js/favorites.js
+++ js/favorites.js
@@ -1,23 +1,36 @@
-const favoriteFactsContainer = document.getElementById("fun-fact");
-const savedFactsKey = "savedFacts";
+document.addEventListener('DOMContentLoaded', () => {
+  const funFactContainer = document.getElementById("fun-fact");
 
-function displayFavoriteFacts() {
-  
-  const savedFacts = localStorage.getItem(savedFactsKey);
-  if (savedFacts) {
-    const favoriteFacts = JSON.parse(savedFacts);
-    favoriteFactsContainer.innerHTML = ""; // Limpiar el contenedor antes de agregar los hechos favoritos
-
-    // Recorrer los hechos curiosos favoritos y crear elementos para mostrarlos
-    favoriteFacts.forEach(fact => {
-      const factElement = document.createElement("p");
-      factElement.textContent = fact;
-      favoriteFactsContainer.appendChild(factElement);
-    });
-  } else {
-    favoriteFactsContainer.textContent = "no favorites yet.";
+  function createFunFactElement(text) {
+    const div = document.createElement("div");
+    div.classList.add("fun-fact-box");
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    div.appendChild(paragraph);
+    return div;
   }
-}
 
-// Llamar a la función para mostrar los hechos curiosos favoritos al cargar la página
-window.addEventListener("DOMContentLoaded", displayFavoriteFacts);
+  function displaySavedFacts(savedFacts) {
+    savedFacts.forEach(factArr => {
+      const fact = factArr[0];
+      const funFactElement = createFunFactElement(fact);
+      funFactContainer.appendChild(funFactElement);
+    });
+  }
+
+  // Obtener datos del localStorage
+  const savedFacts = localStorage.getItem("savedFacts");
+
+  if (savedFacts) {
+    try {
+      const savedFactsJSON = JSON.parse(savedFacts);
+      savedFactsJSON.forEach(factArr => {
+        const fact = factArr[0];
+        const funFactElement = createFunFactElement(fact);
+        funFactContainer.appendChild(funFactElement);
+      });
+    } catch (error) {
+      console.log("Error al analizar el JSON de savedFacts:", error);
+    }
+  }
+});
