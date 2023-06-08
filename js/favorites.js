d7510b884ba672ffebfea7f91c6cbd8d829b1471
Laura <garch.laura@gmail.com>
2023-06-08 10:40:14 +0200

    cross

--- js/favorites.js
+++ js/favorites.js
@@ -1,24 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const funFactContainer = document.getElementById("fun-fact");
-
-  function createFunFactElement(text) {
-    const div = document.createElement("div");
-    div.classList.add("fun-fact-box");
-    const paragraph = document.createElement("p");
-    paragraph.textContent = text;
-    div.appendChild(paragraph);
-    return div;
-  }
-
-  function displaySavedFacts(savedFacts) {
-    savedFacts.forEach(factArr => {
-      const fact = factArr[0];
-      const funFactElement = createFunFactElement(fact);
-      funFactContainer.appendChild(funFactElement);
-    });
-  }
-
-  // Obtener datos del localStorage
+    const funFactContainer = document.getElementById("fun-fact");
+  
+    function createFunFactElement(text) {
+      const div = document.createElement("div");
+      div.classList.add("fun-fact-box");
+      const paragraph = document.createElement("p");
+      paragraph.textContent = text;
+  
+      const crossMark = document.createElement("span");
+      crossMark.classList.add("cross-mark");
+      crossMark.textContent = "x";
+  
+      div.appendChild(crossMark);
+      div.appendChild(paragraph);
+      return div;
+    }
+  
   const savedFacts = localStorage.getItem("savedFacts");
 
   if (savedFacts) {
